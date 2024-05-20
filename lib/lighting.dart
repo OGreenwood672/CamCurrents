9fca937522b0e844f9dcc99d0d703b075b904751
Oliver Fitzgerald <ojbfitzgerald@gmail.com>
2024-05-20 17:07:23 +0100

    Added lighting down and lighting up (2)

--- lib/lighting.dart
+++ lib/lighting.dart
@@ -4,8 +4,26 @@ import 'package:csv/csv.dart';
 import 'package:flutter/services.dart';
 
 
-void loadCSV(String path) async {
+Future<List<List>> _readCSV(String path) async {
   final rawData = await rootBundle.loadString(path);
-  List<List<dynamic>> listData = const CsvToListConverter().convert(rawData);
-  print(listData[0]);
+  List<List<dynamic>> listData = const CsvToListConverter(
+    eol: "\n",
+  ).convert(rawData);
+  return listData;
 }
+
+Future<Map<String, Map<String, dynamic>>> getLighting() async {
+  List<List<dynamic>> listData = await _readCSV("assets/lightings.csv");
+  Map<String, Map<String, dynamic>> result = {};
+  
+  for (List<dynamic> row in listData) {
+    Map<String, dynamic> entry = {};
+    for (int i=1; i<5; i++){
+      entry[listData[0][i]] = row[i];
+    }
+    result[row[0].toString()] = entry;
+  }
+
+  return result;
+}
+
